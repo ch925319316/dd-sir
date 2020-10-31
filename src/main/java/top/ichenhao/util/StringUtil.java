d15851578d08285839f76e2329ddd960aca7f9f6
ch925319316 <925319316@qq.com>
2020-10-31 22:29:01 +0800

    delete

--- src/main/java/top/ichenhao/util/StringUtil.java
+++
@@ -1,7 +0,0 @@
-package top.ichenhao.util;
-
-public class StringUtil {
-    public static boolean isEmpty(CharSequence charSequence) {
-        return charSequence == null || charSequence.length() == 0;
-    }
-}
--- src/main/java/top/ichenhao/util/TimeUtil.java
+++
@@ -1,7 +0,0 @@
-package top.ichenhao.util;
-
-public class TimeUtil {
-    public static String getSecondTime() {
-        return String.format("%010d", new Object[]{Long.valueOf(System.currentTimeMillis() / 1000)});
-    }
-}
